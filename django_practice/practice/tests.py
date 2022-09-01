611bc3cbd0d9c6b3cfcbd19502fad32bb2d7874d
andrei300399 <andrei060656@mail.ru>
2022-09-01 18:58:04 +0300

    add path wdm3

--- django_practice/practice/tests.py
+++ django_practice/practice/tests.py
@@ -86,10 +86,23 @@ class PostTestCase(TestCase):
         self.assertNotIn(post_delete, get_post2)
 
     def test_add_and_delete_post_ui(self):
-        options = Options()
-        options.headless = True
+        chromeOptions = Options()
+        chromeOptions.headless = True
 
-        selenium = webdriver.Chrome(options=options)
+        chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
+        chromeOptions.add_argument("--no-sandbox")
+        chromeOptions.add_argument("--disable-setuid-sandbox")
+
+        chromeOptions.add_argument("--remote-debugging-port=9222")  # this
+
+        chromeOptions.add_argument("--disable-dev-shm-using")
+        chromeOptions.add_argument("--disable-extensions")
+        chromeOptions.add_argument("--disable-gpu")
+        chromeOptions.add_argument("start-maximized")
+        chromeOptions.add_argument("disable-infobars")
+        chromeOptions.add_argument(r"user-data-dir=.\cookies\\test")
+
+        selenium = webdriver.Chrome(options=chromeOptions)
         selenium.get('http://127.0.0.1:8000/')
         input_title = selenium.find_element('id','id_add_title')
         input_text = selenium.find_element('id','id_add_text')
