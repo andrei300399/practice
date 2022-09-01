ad35055f7cd2e8bbfec19dd2e2fe09590c64b44d
andrei300399 <andrei060656@mail.ru>
2022-09-01 21:14:24 +0300

    add path wd24ds2sd2222asd

--- django_practice/django_practice/settings.py
+++ django_practice/django_practice/settings.py
@@ -127,7 +127,7 @@ INSTALLED_APPS += ('django_jenkins',)
 JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
                  'django_jenkins.tasks.run_pep8',
                  'django_jenkins.tasks.run_pyflakes',
-                 'django_jenkins.tasks.django_tests',)
+                )
 
 from .local_settings import *
 SELENIUM_DISPLAY = ':99'
